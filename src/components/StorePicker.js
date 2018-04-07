cf3d0b98572e2b401e0f63735db2a2ff19a56b61
Lauri <lauri@Lauris-MBP.lan>
2018-04-07 14:20:21 +0300

    made method to get ref value from input

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (event) => {
+    event.preventDefault();
+    console.log(this);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please pick your Store:</h2>
-        <input type="text" placeholder="Store name" required></input>
+        <input 
+          type="text"
+          ref={this.myInput} 
+          placeholder="Store name" 
+          defaultValue={getFunName()}
+          required 
+        />
         <button type="submit">Pick your store -></button>
       </form>
     );
