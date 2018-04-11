903f84ba99a53c02e69944b3851955857867fa49
Lauri <lauri@Lauris-MBP.lan>
2018-04-11 23:16:01 +0300

    added PropTypes to all components

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -1,6 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class EditFishForm extends React.Component {
+  static propTypes = {
+    fish: PropTypes.shape({
+      name: PropTypes.string,
+      status: PropTypes.string,
+      desc: PropTypes.string,
+      image: PropTypes.string,
+      price: PropTypes.number
+    }),
+    index: PropTypes.string,
+    updateFish: PropTypes.func,
+    deleteFish: PropTypes.func
+  }
+
   handleChange = (event) => {
     const updatedFish = { 
       ...this.props.fish,
